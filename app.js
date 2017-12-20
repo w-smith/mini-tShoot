8ad641372e528b6fa8e93c060f214b19218f90ec
w-smith <wsmith@bex.net>
2017-12-20 09:31:31 -0700

    done

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 function addProjects() {
-	var projectDiv = document.getElementById("#ourProjects");
+	var projectDiv = document.getElementById("ourProjects");
 	for (var i =0; i < 4; i++) {
 		var project = document.createElement("div");
 		var projectLink = document.createElement("a");
 		var projectImage = document.createElement("img");
-		projectLink.setAttribute("href", "http://www.zombo.com");
-		projectImage.setAttribute("src", "project"+1+".png");
+		projectLink.setAttribute("href", "http://www.github.com/w-smith");
+		projectImage.setAttribute("src", "project"+i+".png");
 		projectLink.appendChild(projectImage);
 		project.appendChild(projectLink);
 		projectDiv.appendChild(project);
